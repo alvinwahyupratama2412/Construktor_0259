eb1c83fe36cc543a957c6401319b72fe0a821435
alvinwahyupratama2412 <alvinwahyupratama22@gmail.com>
2025-05-13 10:11:30 +0700

    first commit

---
+++ Latihan.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+class persegiPanjang;
+class bangunDatar{
+  public: 
+  int hitungLuas(int l, int p);
+  int hitungkeliling(int l, int p);
+};
+
+class persegiPanjang{
+  public : 
+  int panjang,lebar;
+    friend int bangunDatar::hitungLuas(int l, int p);
+    friend int bangunDatar::hitungkeliling(int l, int p);
+};
+
+int bangunDatar::hitungLuas(int l, int p){
+    return l*p;
+}
+int bangunDatar::hitungkeliling(int l, int p){
+    return (2*p)+(2*l);
+}
+
+int main(){
+    bangunDatar bD;
+    persegiPanjang pP;
+    persegiPanjang &refpP = pP;
+    refpP.panjang = 6;
+    refpP.lebar = 3;
+    cout << "Luasnya = " << bD.hitungLuas(refpP.lebar, refpP.panjang)<<endl;
+}
