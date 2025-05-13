91774de68bec7fb68411fa1667a097917ec24154
alvinwahyupratama2412 <alvinwahyupratama22@gmail.com>
2025-05-13 10:57:49 +0700

    membuat objek mhs3 yang berisi nim dan nama serta memanggilnya menggunakan prosedur cetak

--- constructoroverloading.cpp
+++ constructoroverloading.cpp
@@ -38,7 +38,9 @@ void mahasiswa::cetak(){
 int main(){
     mahasiswa mhs1(102030);         //hanya nim
     mahasiswa mhs2("Andi");         //hanya nama
+    mahasiswa mhs3(010203, "Santi");//Nim dan Nama
 
     cout << "Mahasiswa 1: "; mhs1.cetak();
     cout << "Mahasiswa 2: "; mhs2.cetak();
+    cout << "Mahasiswa 3: "; mhs3.cetak();
 }
